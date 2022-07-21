d18f06c0fdad822f35043edda8a76d9e6ee873ff
Kaio Costa de Oliveira <hello@kaio.dev>
2022-07-21 20:31:50 -0300

    atualiza método _validateEmail

--- lib/main.dart
+++ lib/main.dart
@@ -61,8 +61,27 @@ class _HomeState extends State<Home> {
   }
 
   String? _validateEmail(String? value) {
+    final upperCase = RegExp(r'[A-Z]');
+    final rfc5322 = RegExp(
+        r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])+");
+
     if (value == null || value.isEmpty) {
       return 'o campo não pode ser vazio';
+    }
+    //Verificação com hasMatch
+    // if (upperCase.hasMatch(value)) {
+    //   return 'o campo não pode ter letras maiúsculas';
+    // }
+
+    if (value.contains(upperCase)) {
+      return 'o campo não pode ter letras maiúsculas';
+    }
+
+    if (value.contains(' ')) {
+      return 'o campo não pode conter espaços';
+    }
+    if (!value.contains(rfc5322)) {
+      return 'email inválido, digite um email corretamente';
     }
     return null;
   }
