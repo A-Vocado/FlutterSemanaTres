dbfae8a2db5b80253c095a4229021a7d3a0c9ae7
Hazelpony <hazelpony88@gmail.com>
2022-07-26 08:27:20 -0300

    Create confirmacao_field.dart

---
+++ lib/confirmacao_field.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:formy/validator.dart';
+
+class ConfirmacaoField extends StatelessWidget {
+  const ConfirmacaoField({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: TextFormField(
+        decoration: const InputDecoration(labelText: 'Confirme sua senha'),
+        keyboardType: TextInputType.visiblePassword,
+        validator: Validator.validateConfirmacao,
+        onSaved: (value) {
+          debugPrint('enviando dados para api');
+        },
+      ),
+    );
+  }
+}
