d7cb0f6ca0c8d6ad5e056bd7f5fd726fee04f057
Hazelpony <hazelpony88@gmail.com>
2022-07-26 05:09:55 -0300

    Update nome_field.dart

---
+++ lib/nome_field.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:formy/validator.dart';
+
+class EmailField extends StatelessWidget {
+  const EmailField({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: TextFormField(
+        decoration: const InputDecoration(labelText: 'Nome'),
+        keyboardType: TextInputType.emailAddress,
+        validator: Validator.validateEmail,
+        onSaved: (value) {
+          debugPrint('enviando dados para api');
+        },
+      ),
+    );
+  }
+}
